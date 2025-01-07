7e83e3b049d5d77e091e9ef481d157c844c98b8b
DuVC4310 <vfranciscoeduardo055@gmail.com>
2025-01-07 19:33:30 -0300

    Add files via upload

---
+++ Par ou impar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    int num;
+
+    printf("informe um numero:\n");
+    scanf("%d", &num);
+
+    if (num % 2==0){
+     printf("Numero par,\n");  
+    }else
+    printf("Numero impar,\n");
+     
+
+}
